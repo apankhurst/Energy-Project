2a68adbbc07fdeeb32160cebc77b356755a6e5ff
apankhurst <alexspankhurst@gmail.com>
2017-12-03 15:45:53 -0500

    ratepayer module

--- ratepayer.py
+++ ratepayer.py
@@ -4,7 +4,7 @@ import flask
 from flask import Flask
 from pymongo import MongoClient
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 database_name = 'db'
 
@@ -14,5 +14,13 @@ appliance_db = client[database_name]
 # allow appliance to update the data base
 # data will come in the form
 # appliance_id, start_time, end_time, energy_usage
-def post_data(self, app_type, name, start, end, value):
-        
+#def post_data(self, app_type, name, start, end, value):
+
+@app.route('/')
+def test_method():
+    return 'Hi Ari'
+
+@app.route('/hello/<name>')
+def test_name(name):
+    return 'hello ' + str(name)
+
