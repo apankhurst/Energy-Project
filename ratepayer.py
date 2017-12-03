efa233040cfc56bff9a66ce0b9fb9f4fd6311b2f
apankhurst <alexspankhurst@gmail.com>
2017-12-03 15:05:09 -0500

    ratepayer stuff

--- ratepayer.py
+++ ratepayer.py
@@ -1,6 +1,12 @@
 import pymongo
 import flask
 
+from pymongo import MongoClient
+
+database_name = 'db'
+
 class RatePayer:
     def __init__(self):
-        
+        # Create a client and get the database
+        self.client = MongoClient()
+        self.appliace_db = self.client['db']
