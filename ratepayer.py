ef537a4a0e5000cde373688c8d38394f9c17e673
apankhurst <alexspankhurst@gmail.com>
2017-12-03 15:10:28 -0500

    stuff

--- ratepayer.py
+++ ratepayer.py
@@ -6,7 +6,15 @@ from pymongo import MongoClient
 database_name = 'db'
 
 class RatePayer:
+
+    # Ratepayer constructor
     def __init__(self):
-        # Create a client and get the database
+        # create a client and get the database
         self.client = MongoClient()
         self.appliace_db = self.client['db']
+
+    # post data to the database
+    def post_data(app_type, name, start, end, value):
+
+    def get_by_id(app_id, start_time, end_time):
+        
