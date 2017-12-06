cf2fde6506b8cc183f173a4a5553d9a913378e7e
lucienbaumann <l@baukin.com>
2017-12-05 21:32:09 -0500

    Merge branch 'master' of /Users/luke/PycharmProjects/Energy-Project with conflicts.

--- ratepayer.py
+++ ratepayer.py
@@ -1,7 +1,5 @@
 import pymongo
 import flask
-import json
-import checking
 
 from flask import Flask
 from flask import request
@@ -14,7 +12,6 @@ database_name = 'db'
 client = MongoClient("mongodb://root:CyeX6L2e19AT@ec2-54-165-229-239.compute-1.amazonaws.com:27017")
 db = client['ratepayer_db']
 applicance_collection = db['appliances']
-#print(appliance_db.get_collection('app'))
 
 # allow appliance to update the data base
 # data will come in the form
@@ -29,28 +26,15 @@ def test_method():
 def test_name(name):
     return 'hello ' + str(name)
 
-# allow home appliances to submit data to the ratepayer with the REST API
 @app.route('/submit')
 def record_data():
-    # get the passed parameters
     appliance_id = request.args.get('id')
     start_date = request.args.get('start')
     end_date = request.args.get('end')
     energy = request.args.get('energy')
 
-    # check all parameters exist
-    if (appliance_id is None or
-        start_date is None or
-        end_date is None or energy is None):
+    if appliance_id is None or start_date is None or end_date is None or energy is None:
         return 'ARGUMENT ERROR'
-
-    
-    # check if all parameters are valid
-    if (!(valid_datetime(start_date)) or
-        !(valid_datetime(end_date)) or
-        start_date >= end_date or energy < 0):
-        return 'ARGUMENT ERROR'
-    
 
     record = {
         'start': start_date,
@@ -59,11 +43,11 @@ def record_data():
     }
 
     try:
-        this_appliance = db.get_collection(appliance_id + str())
-        this_appliance.insert_one(record)
+        result = db[appliance_id].insert(record)
 
     except:
         print("Error")
+    print(x for x in db.collection_names())
 
-    retval = appliance_id
+    retval = str(db.collection_names())
     return retval
