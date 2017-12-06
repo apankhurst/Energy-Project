76e0b5115ffd741390cff4b732bcf85b256d84db
lucienbaumann <l@baukin.com>
2017-12-05 22:21:38 -0500

    Fixed DB

--- ratepayer.py
+++ ratepayer.py
@@ -3,30 +3,40 @@ import flask
 import json
 import optparse
 import checking
-
+from urllib.parse import quote_plus
 from flask import Flask
 from flask import request
 from pymongo import MongoClient
+from checking import valid_datetime
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb://root:CyeX6L2e19AT@ec2-54-165-229-239.compute-1.amazonaws.com:27017")
+username = "root"
+password = "CyeX6L2e19AT"
+hostname = ""
+uri = "mongodb://%s:%s@ec2-54-165-229-239.compute-1.amazonaws.com/ratepayer_db?authsource=admin" % (
+    quote_plus(username), quote_plus(password))
+# Should remove connect=True after testing is done
+client = MongoClient(uri)
+
 db = client['ratepayer_db']
 applicance_collection = db['appliances']
-#print(appliance_db.get_collection('app'))
+
 
 # allow appliance to update the data base
 # data will come in the form
 # appliance_id, start_time, end_time, energy_usage
-#def post_data(self, app_type, name, start, end, value):
+# def post_data(self, app_type, name, start, end, value):
 
 @app.route('/')
 def test_method():
     return 'Hi Ari'
 
+
 @app.route('/hello/<name>')
 def test_name(name):
     return 'hello ' + str(name)
+
 
 # allow home appliances to submit data to the ratepayer with the REST API
 @app.route('/submit')
@@ -39,38 +49,34 @@ def record_data():
 
     # check all parameters exist
     if (appliance_id is None or
-        start_date is None or
-        end_date is None or energy is None):
-        return 'ARGUMENT ERROR'
+                start_date is None or
+                end_date is None or energy is None):
+        return 'ARGUMENT ERROR: Missing arguments'
 
-    
     # check if all parameters are valid
     if (not valid_datetime(start_date) or
-        not valid_datetime(end_date) or
-        start_date >= end_date or energy < 0):
-        return 'ARGUMENT ERROR'
-    
+            not valid_datetime(end_date) or
+                start_date >= end_date or int(energy) < 0):
+        return 'ARGUMENT ERROR: Invalid dates'
 
-    record = {
+
+    try:
+        this_appliance = db.get_collection(str(appliance_id))
+        result = this_appliance.insert_one({
         'start': start_date,
         'end': end_date,
         'energy': energy
-    }
-
-    try:
-        this_appliance = db.get_collection(appliance_id + str())
-        this_appliance.insert_one(record)
-
+    })
     except:
         print("Error")
 
     retval = appliance_id
     return retval
 
-def get_database_info(info_file):
 
-    # json file that stores all of the info for the 
+def get_database_info(info_file):
+    # json file that stores all of the info for the
     with open(info_file) as json_data:
         database_info = json.load(json_data)
 
-    return database_info  
+    return database_info
