fa5f36872cb85ad94ab1ee32fae6d120d8d08aca
apankhurst <alexspankhurst@gmail.com>
2017-12-05 21:16:33 -0500

    got valid_datetime working

--- checking.py
+++ checking.py
@@ -1,21 +1,21 @@
 # this is a module that implements functions for checking formats of parameters
 from datetime import datetime
 
+# check if a passed date and time is value
+# a datetime will be invalid if it doesn't fit the standard format or if it is
+# larger than the current date
 def valid_datetime(dt):
+
+    # the ISODate format string
     dt_format = "%Y-%m-%dT%H:%M"
 
+    # check date and time meet the necessary format
     try:
         datetime.strptime(dt, dt_format)
     except ValueError:
         return False
 
+    # check the date & time are not in the future
     current = datetime.now().strftime(dt_format)
-    
-    return dt <= current
 
-def tests():
-    test_1 = "2012-12-19T06:01"  
-    print(test_1 + " : " + str(valid_datetime(test_1)))
-    test_2 = "2018-12-19T06:01"
-    print(test_2 + " : " + str(valid_datetime(test_1)))
-tests()
+    return dt <= current
--- ratepayer.py
+++ ratepayer.py
@@ -1,5 +1,7 @@
 import pymongo
 import flask
+import json
+import checking
 
 from flask import Flask
 from flask import request
@@ -12,7 +14,7 @@ database_name = 'db'
 client = MongoClient("mongodb://root:CyeX6L2e19AT@ec2-54-165-229-239.compute-1.amazonaws.com:27017")
 db = client['ratepayer_db']
 applicance_collection = db['appliances']
-print(appliance_db.get_collection('app'))
+#print(appliance_db.get_collection('app'))
 
 # allow appliance to update the data base
 # data will come in the form
@@ -27,15 +29,28 @@ def test_method():
 def test_name(name):
     return 'hello ' + str(name)
 
+# allow home appliances to submit data to the ratepayer with the REST API
 @app.route('/submit')
 def record_data():
+    # get the passed parameters
     appliance_id = request.args.get('id')
     start_date = request.args.get('start')
     end_date = request.args.get('end')
     energy = request.args.get('energy')
 
-    if appliance_id is None or start_date is None or end_date is None or energy is None:
+    # check all parameters exist
+    if (appliance_id is None or
+        start_date is None or
+        end_date is None or energy is None):
         return 'ARGUMENT ERROR'
+
+    
+    # check if all parameters are valid
+    if (!(valid_datetime(start_date)) or
+        !(valid_datetime(end_date)) or
+        start_date >= end_date or energy < 0):
+        return 'ARGUMENT ERROR'
+    
 
     record = {
         'start': start_date,
