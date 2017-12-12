030a5fdc6fb83dbc58d20dcff03e13508ff868c1
apankhurst <alexspankhurst@gmail.com>
2017-12-12 16:10:22 -0500

    did city stuff

--- city.py
+++ city.py
@@ -1,53 +1,35 @@
-import sys
 import flask
 import json
-import csv
+import requests
 
 from flask import Flask
 from flask import request
 
 # This file contains a list of the IPs
 # of the rate payers below the city
-ratepayer_ips = []
+ratepayers = {}
 
-config_file = "test.txt"
-open_file = open(config_file, "r")
+init_file = "city_config.json"
+with open(init_file) as json_data:
+    info = json.load(json_data)
+    
+    for r in info['ratepayers']:
+        ip = r['ip']
+        port = r['port']
+        ratepayers[ip] = port
 
-
-
-
-
-
-
-
-
+for r in ratepayers:
+    print(r + " " + ratepayers[r])
+        
 app = Flask(__name__)
 
-@app.route('/submit')
-def record_data():
-    appliance_id = request.args.get('id')
-    start_date = request.args.get('start')
-    end_date = request.args.get('end')
-    energy = request.args.get('energy')
+@app.route('/total')
+def total():
+    total = 0.0
+    for rp in ratepayers:
+        host_str = 'http://' + rp + ":" + ratepayers[rp] + "/total"
+        print(host_str)
 
-    if appliance_id is None or start_date is None or end_date is None or energy is None:
-        return 'ARGUMENT ERROR'
-
-    record = {
-        'start': start_date,
-        'end': end_date,
-        'energy': energy
-    }
-
-    try:
-        this_appliance = db.get_collection(appliance_id + str())
-        this_appliance.insert_one(record)
-
-    except:
-        print("Error")
-
-    retval = appliance_id
-    return retval
 
 if __name__ == '__main__':
     app.run()
