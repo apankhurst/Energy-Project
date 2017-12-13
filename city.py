679fc2d2eaced616458620614b70579f786e489b
apankhurst <alexspankhurst@gmail.com>
2017-12-12 22:30:42 -0500

    worked on city

--- city.py
+++ city.py
@@ -4,6 +4,7 @@ import requests
 
 from flask import Flask
 from flask import request
+from checking import valid_datetime
 
 # This file contains a list of the IPs
 # of the rate payers below the city
@@ -25,10 +26,18 @@ app = Flask(__name__)
 
 @app.route('/total')
 def total():
+
+    start = valid_datetime(request.args.get('start'))
+    end = valid_datetime(request.args.get('end'))
+
+    start_str = start.strftime("%Y-%m-%dT%H:%M")
+    end_str = end.strftime("%Y-%m-%dT%H:%M")
+    
     total = 0.0
     for rp in ratepayers:
-        host_str = 'http://' + rp + ":" + ratepayers[rp] + "/total"
-        print(host_str)
+        url_str = 'http://' + rp + ":" + ratepayers[rp] + "/total?"
+        payload = "start="+start_str+"&end="end_str
+        print(url_str+payload)
 
 
 if __name__ == '__main__':
