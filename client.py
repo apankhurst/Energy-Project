e0797231b0ea29b869042dbe724e0abe068bf474
apankhurst <alexspankhurst@gmail.com>
2017-12-14 16:50:49 -0500

    client stuff

--- client.py
+++ client.py
@@ -1,10 +1,23 @@
+"""
+- this module provides a CLI for inteacting
+with the DEDASS system
+- all information regarding the levels that can
+be contacted must be stored with in a .json file
+"""
+
+
 import json
 import requests
 import sys
 
 from checking import valid_datetime
 
+levels = {}
+
+# check if there exists an element called name at the specified
+# level
 def get_by_name(level, name):
+    # check level exists
     if levels[level] is None:
         print("level doesn't exist")
         return {}
@@ -12,28 +25,36 @@ def get_by_name(level, name):
     lev = levels[level]
     select = {}
     
+    # check name exists
     for l in lev:
         if l['name'] == name:
             select = l
             break
-        
+    
     if select == {}:
         print(name + " doesn't exist")
+
+    # return the element dictionary
     return select
 
+# interact with a DEDASS endpoint
 def contact_endpoint(endpoint,level,name,filters):
+
+    # get information on level element
     select = get_by_name(level,name)
     if select == {}:
         return 
-    
+
+    # ip address and port to contact
     ip = select['ip']
     port = select['port']
-    
+
+    # get start and end dates
     try:
         print("date format: %Y-%m-%dT%H:%M")
         s = input('please enter a start date: ')
         start = valid_datetime(s)
-        
+
         e = input('please enter an end date: ')
         end = valid_datetime(s)
 
@@ -41,49 +62,61 @@ def contact_endpoint(endpoint,level,name,filters):
             print('please make sure your dates are in the right order')
             return
 
+        # construct the endpoint and payload 
         url_str = "http://"+ip+":"+port+endpoint
         payload = "?start="+s+"&end="+e
 
         if filters is not None:
             payload += filters
 
-
+        # contact the endpoint   
         print(url_str+payload)
         r = requests.get(url_str+payload)
         return r.json
-        
+
+
     except ValueError:
         return 
 
+# print the available levels
 def print_levels():
     for level in levels:
         if levels[level]:
             print(level)
 
+# print elements available at a level
 def view(level):
-    for l in levels[level]:
-        print(l['name'])
+    try:
+        for l in levels[level]:
+            print(l['name'])
+    except KeyError:
+        print('no level with name ' + level)
 
-
+# get the total # of devices for a level and
+# their total energy usage
 def energy(level,name,appliances):
     filters = None
-    if appliances:
+
+    if len(appliances) > 0:
         filters = "&types="+",".join(appliances)
     
     response = contact_endpoint("/appliances/all",level,name,filters)
-    print("Total Devices: " + response['appliances_count'])
-    print("Total Energy Usage: " + response['total_energy'])
-    print("Average Energy Usage: " + response['average_energy'])
+    print("total Devices: " + response['appliances_count'])
+    print("total Energy Usage: " + response['total_energy'])
+    print("average Energy Usage: " + response['average_energy'])
 
+# get the total # of devices for a level and
+# their total power usage
 def power(level,name,appliances):
     filters = '&power=True'
-    if appliances:
-        filters = "&types="+",".join(appliances)
-        
-        response = contact_endpoint("/appliances/all",level,name,filters)
-        print("Total Devices: " + response['appliances_count'])
-        print("Total Power Usage: " + response['total_power'])
-        
+    if len(appliances) > 0:
+        filters += "&types="+",".join(appliances)
+
+    response = contact_endpoint("/appliances/all",level,name,filters)
+    print("total Devices: " + response['appliances_count'])
+    print("total Power Usage: " + response['total_power'])
+
+# print commands
 def help():
     print('levels - print the levels available for query')
     print('view <level> - view all available elements at <level>')
@@ -94,8 +127,7 @@ def help():
 if len(sys.argv) < 2:
     print('usage: python client.py <config.json>')
 
-levels = {}
-
+# setup from config file
 config_file = sys.argv[1]
 with open(config_file) as json_data:
     info = json.load(json_data)
@@ -116,7 +148,10 @@ print(" . . .T. . ")
 print(" . . .I. . ")
 print(" . . .O. . ")
 print(" . . .N. . ")
-    
+
+print("\nfor help type help")
+
+# get user input and perform query
 while True:
     try:
         ans = input('> ').split(' ')
